89117c979d717d9af4d14df9065ae42cb0621bb9
Adam Dale <madale2010@users.noreply.github.com>
2019-03-09 12:36:10 -0500

    Update ZipCode.java

--- usps/src/main/java/com/madbox/usps/ZipCode.java
+++ usps/src/main/java/com/madbox/usps/ZipCode.java
@@ -1,3 +1,11 @@
+/*******************************************************
+ * Copyright (C) 2012-2019 Adam Dale adamdale2018@gmail.com
+ * 
+ * This file is part of Madbox Solutions.
+ * 
+ * USPS Java Api can not be copied and/or distributed without the express
+ * permission of Madbox Solutions.
+ *******************************************************/
 package com.madbox.usps;
 
 import javax.xml.bind.annotation.XmlAttribute;
@@ -5,10 +13,6 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
-/**
- * @author madal
- *
- */
 @XmlRootElement(name="ZipCode")
 @XmlType(propOrder = {"zip5","city","state"})
 public class ZipCode {
